ed4f34b2b0aea653c181054a24fc62a141b8d8fd
Eleftherios Papadopoulos <lefteris.broker@gmail.com>
2024-07-16 22:00:27 +0200

    kubernetes

--- system/deploy.sh
+++ system/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Apply Kubernetes configurations
+# Apply Kubernetes configurations for frontend
 echo "Applying Kubernetes configurations for frontend..."
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/frontend-namespace.yaml
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/frontend-deployment.yaml
@@ -12,18 +12,22 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+# Apply Kubernetes configurations for backend
 echo "Applying Kubernetes configurations for backend..."
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/backend-namespace.yaml
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/backend-deployment.yaml
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/backend-service.yaml
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/postgres-deployment.yaml
+microk8s kubectl apply -f /home/sellinios/aethra/microk8s/database-secret.yaml
 
 if [ $? -ne 0 ]; then
   echo "Failed to apply backend Kubernetes configurations. Exiting."
   exit 1
 fi
 
+# Apply Kubernetes configurations for ingress
 echo "Applying Kubernetes configurations for ingress..."
+microk8s kubectl apply -f /home/sellinios/aethra/microk8s/ingress-namespace.yaml
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/nginx-ingress-service.yaml
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/cluster-issuer.yaml
 microk8s kubectl apply -f /home/sellinios/aethra/microk8s/tls-certificate.yaml
