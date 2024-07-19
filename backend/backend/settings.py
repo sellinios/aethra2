70687d986226d6e0bcbd6bf277687eb85c8ce776
Eleftherios Papadopoulos <lefteris.broker@gmail.com>
2024-07-19 19:48:49 +0200

    ready

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,11 +66,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.getenv('POSTGRES_DB', 'aethradb'),
-        'USER': os.getenv('POSTGRES_USER', 'sellinios'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'faidra123!@#'),
-        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
